d87eaf53c1eafc18c6f66a8719e4ae19d37f5799
GuilhermedeMoura <63135753+GuilhermedeMoura@users.noreply.github.com>
2023-10-12 21:31:45 -0300

    first commit

---
+++ Docker/app.py
@@ -0,0 +1,32 @@
+import http.server
+import socketserver
+from flask import Flask
+
+PORT = 8080
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!\n"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!\n"
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8080)
